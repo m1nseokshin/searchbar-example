5fb1858e16ec84c7a860d3e1d187749cdd3fd557
m1nseokshin <kevin5411@naver.com>
2023-07-01 16:35:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ $(function() {
     });
 
 });
+
+$(function() {
+    $('.background').on('click', function(event){
+        event.preventDefault();
+            
+        $(this).toggleClass('active');
+        $('.overlay').toggleClass('visible');
+        $('.background').toggleClass('visible');
+        $('body').toggleClass('active');
+    });
+
+});
